4996ee83898ca3ccf04bef1d071574dc5d961b1c
nick <nick3olden@gmail.com>
2021-11-07 14:23:32 +0500

    Change name

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,7 @@ const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
       <a className="navbar-brand js-scroll-trigger" href="#about">
-        <span className="d-block d-lg-none">Clarence Taylor</span>
+        <span className="d-block d-lg-none">Николай Пискунов</span>
         <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="profile" /></span>
       </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span onClick={toggleMenu} className="navbar-toggler-icon"/></button>
